392c40d1ad5509ac35b8e344d7eeda41d2c6757a
LDH <343091787@qq.com>
2017-08-17 17:35:56 +0800

    开始进行新架构的工作

---
+++ timeTrader/environment.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 15:32:38 2017
+
+@author: LDH
+"""
+
+# environment.py
+
+class Environment():
+    '''
+    环境。
+    '''
+    _environment = None
+    
+    def __init__(self,config):
+        '''
+        事件驱动的回测/模拟环境。
+        
+        Parameters
+        ----------
+        config
+            回测环境设定。包括
+                start_date:开始日期
+                end_date:结束日期
+                initial_cash:起始资金
+                frequency:'1d',回测/模拟 类型
+                rebalance_frequency:'5',调仓频率         
+        '''
+        Environment._environment = self
+        self.config = config
+        self.envent_bus = None
+        self.event_source = None
+        self._universe = None
+        self.context = None
+        self.data_proxy = None
+        self.blotter = None
+        self.record = None
+        self.canlendar = None
+        self.calendar_datetime = None
+        self.trading_datetime = None
+        
+    @classmethod
+    def get_instance(cls):
+        return Environment._environment
+    
+    def set_event_bus(self,event_bus):
+        self.event_bus = event_bus
+        
+    def set_event_source(self,event_source):
+        self.event_source = event_source
+        
+    def set_universe(self,universe):
+        self._universe = universe
+        
+    def set_context(self,context):
+        self.context = context
+        
+    def set_data_proxy(self,data_proxy):
+        self.data_proxy = data_proxy
+        
+    def set_blotter(self,blotter):
+        self.blotter = blotter
+        
+    def set_recorder(self,recorder):
+        self.recorder = recorder
+        
+    def set_calendar(self,calendar):
+        self.calendar = calendar
+        
+if __name__ == '__main__':
+    env = Environment()
+    if env == Environment.get_instance():
+        print('Successful')
+    
+    
+        
